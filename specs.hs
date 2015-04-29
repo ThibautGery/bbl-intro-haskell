42e17f493af9bd6edb76c13e94a7b7ca8576f39f
Thibaut Géry <thibaut.gery@gmail.com>
2015-04-29 14:14:00 +0200

    add a recursion implementation that terminate

--- specs.hs
+++ specs.hs
@@ -2,11 +2,20 @@ module Main
 where
 import Test.Hspec
 
+reverseRecursionWithAcc :: String -> String -> String
+reverseRecursionWithAcc acc []  = acc
+reverseRecursionWithAcc acc (a:q) = reverseRecursionWithAcc (a : acc) q
+
+reverseRecursionTerminate :: String -> String
+reverseRecursionTerminate = reverseRecursionWithAcc ""
+
 reverseRecursion :: String -> String
 reverseRecursion [] = []
 reverseRecursion (a:b) = (reverseRecursion b) ++ [a]
 -- reverseRecursion l = (reverseRecursion (tail l)) ++ [head l]
 -- reverseRecursion l = (reverseRecursion $ tail l) ++ [head l]
+
+
 
 main = hspec $ do
     describe "hello world" $ do
@@ -16,13 +25,17 @@ main = hspec $ do
     describe "reverse string" $ do
         it " \"\"-> \"\"" $ do
           reverseRecursion "" `shouldBe` ""
+          reverseRecursionTerminate "" `shouldBe` ""
 
         it " a -> a " $ do
           reverseRecursion "a" `shouldBe` "a"
+          reverseRecursionTerminate "a" `shouldBe` "a"
 
 
         it " ab -> ba " $ do
           reverseRecursion "ab" `shouldBe` "ba"
+          reverseRecursionTerminate "ab" `shouldBe` "ba"
 
         it " abc -> cba " $ do
           reverseRecursion "abc" `shouldBe` "cba"
+          reverseRecursionTerminate "abc" `shouldBe` "cba"
