e1b9c0dce41bccc5728c7ecff22e16902d19c4ed
Thibaut Géry <thibaut.gery@gmail.com>
2015-04-29 14:14:22 +0200

    add the fold implementation

--- specs.hs
+++ specs.hs
@@ -15,7 +15,8 @@ reverseRecursion (a:b) = (reverseRecursion b) ++ [a]
 -- reverseRecursion l = (reverseRecursion (tail l)) ++ [head l]
 -- reverseRecursion l = (reverseRecursion $ tail l) ++ [head l]
 
-
+reverseFold :: String -> String
+reverseFold = foldl (\ acc a -> a : acc ) ""
 
 main = hspec $ do
     describe "hello world" $ do
@@ -26,16 +27,20 @@ main = hspec $ do
         it " \"\"-> \"\"" $ do
           reverseRecursion "" `shouldBe` ""
           reverseRecursionTerminate "" `shouldBe` ""
+          reverseFold "" `shouldBe` ""
 
         it " a -> a " $ do
           reverseRecursion "a" `shouldBe` "a"
           reverseRecursionTerminate "a" `shouldBe` "a"
+          reverseFold "a" `shouldBe` "a"
 
 
         it " ab -> ba " $ do
           reverseRecursion "ab" `shouldBe` "ba"
           reverseRecursionTerminate "ab" `shouldBe` "ba"
+          reverseFold "ab" `shouldBe` "ba"
 
         it " abc -> cba " $ do
           reverseRecursion "abc" `shouldBe` "cba"
           reverseRecursionTerminate "abc" `shouldBe` "cba"
+          reverseFold "abc" `shouldBe` "cba"
