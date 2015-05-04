d57f3cea433db0286cb01349a0d879524c6afadb
Thibaut Géry <thibaut.gery@gmail.com>
2015-05-04 13:36:00 +0200

    recupération des n premiers nombres premiers

--- nombrePremier.hs
+++ nombrePremier.hs
@@ -10,6 +10,10 @@ estNombrePremier n = all (\ a ->  estPasDivisible n a) [2..n-1]
 estPasDivisible :: Int -> Int -> Bool
 estPasDivisible a b = (a `mod` b /= 0)
 
+recupererLesPremiersPremiers :: Int -> [Int]
+recupererLesPremiersPremiers n = take n [i | i <- [2..], estNombrePremier i]
+
+
 
 main = hspec $ do
     describe "hello world" $ do
@@ -28,3 +32,9 @@ main = hspec $ do
 
         it " nombre premier 4 should be false" $ do
           estNombrePremier 4 `shouldBe` False
+
+    describe "récupérer les n premiers nombres premiers" $ do
+        it " récupérer les deux premiers premiers -> 2, 3" $ do
+          recupererLesPremiersPremiers 2 `shouldBe` [2, 3]
+        it " récupérer les trois premiers premiers -> 2, 3, 5" $ do
+          recupererLesPremiersPremiers 3 `shouldBe` [2, 3, 5]
